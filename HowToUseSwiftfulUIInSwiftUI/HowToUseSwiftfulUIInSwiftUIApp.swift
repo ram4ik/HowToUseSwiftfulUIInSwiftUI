8112ebe1d90d1caff0108a8085354d98f641d1f3
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-19 17:47:56 +0200

    Initial Commit

---
+++ HowToUseSwiftfulUIInSwiftUI/HowToUseSwiftfulUIInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseSwiftfulUIInSwiftUIApp.swift
+//  HowToUseSwiftfulUIInSwiftUI
+//
+//  Created by Ramill Ibragimov on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseSwiftfulUIInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
